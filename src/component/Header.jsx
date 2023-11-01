3f0b39c5291132f094981a89274306460eadb74b
leen <leenodeh287@gmail.com>
2023-11-01 23:52:16 +0300

    update state-user

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect,useState } from "react";
 import { Link, NavLink, Navigate, useNavigate } from "react-router-dom";
 import { ThemeContext } from "../Context/ThemeContext";
 import "./Header.scss";
@@ -7,10 +7,20 @@ import { auth } from "../Firebase/Config";
 import { getAuth, signOut } from "firebase/auth";
 
 
+
+
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate=useNavigate();
   const [user, loading, error] = useAuthState(auth);
   const { dark, toggle } = useContext(ThemeContext);
+  useEffect(() => {
+    if (user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [user]);
 
   const c = dark ? "light" : "dark";
   return (
@@ -28,13 +38,13 @@ const Header = () => {
       </Link>
       <button onClick={toggle}>Toggle</button>
       <div>
-        {!user && (
+        {!isLoggedIn && (
           <>
             <NavLink to="/Login">Sign in </NavLink>
-            <NavLink to="/Register">Sign up </NavLink>{" "}
+            <NavLink to="/Register">Sign up </NavLink>
           </>
         )}
-        {user && (
+        { isLoggedIn && (
           <>
             <NavLink to="/html">HTML </NavLink>
             <NavLink to="/css" style={{ margin: "0px 20px" }}>
@@ -43,6 +53,7 @@ const Header = () => {
             <NavLink to="/js"> JS</NavLink>
             <NavLink to="/profile" style={{margin:"0px 10px"}}>Profile</NavLink>
             <NavLink to="/login" onClick={()=>{
+
               const auth = getAuth();
               navigate("/Login");
               signOut(auth).then(() => {
