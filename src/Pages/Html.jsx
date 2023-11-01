860919159ce11a1d20fd8a42bfedd1095bc338e7
leen <leenodeh287@gmail.com>
2023-11-01 23:21:01 +0300

    add useeffect to prevent show pages if !user

--- src/Pages/Html.jsx
+++ src/Pages/Html.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect } from "react";
 import Main from "../component/Main";
 import "./Ja.scss";
 import { ThemeContext } from "../Context/ThemeContext";
+import { useNavigate } from "react-router-dom";
+import {auth} from '../Firebase/Config'
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 const Html= () => {
   const { dark } = useContext(ThemeContext);
-
+  const navigate=useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user]);
   const c = dark ? "light" : "dark";
   return (
     <div
