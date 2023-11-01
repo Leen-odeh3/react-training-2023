5f7a0775ff7eac229b48f10ba0f75e86fb641617
leen <leenodeh287@gmail.com>
2023-11-01 19:01:44 +0300

    update input-fieald

--- src/Forms/Login.jsx
+++ src/Forms/Login.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <div style={{height:"585px",textAlign:"center"}}>
-    <input type="text" placeholder='plz enter your email'/>
-    <input type="text" placeholder='plz enter your email'/>
+    <input type="text" placeholder='plz enter your email' required/>
+    <input type="password" placeholder='plz enter your password' required/>
     <button >LogIn</button>
 <p>Dont have an account <Link to="/Register">Register</Link></p>
     </div>
--- src/Forms/Register.jsx
+++ src/Forms/Register.jsx
@@ -6,10 +6,10 @@ const Register = () => {
   return (
     <div style={{height:"585px",textAlign:"center"}}>
       <p>Create an account</p>
-      <input type="text" placeholder='plz enter your name'/>
+      <input type="text" placeholder='plz enter your name' required/>
 
-    <input type="text" placeholder='plz enter your email'/>
-    <input type="text" placeholder='plz enter your email'/>
+    <input type="text" placeholder='plz enter your email' required/>
+    <input type="password" placeholder='plz enter your password' required/>
     <button >SignUp</button>
 <p> Already have an account<Link to="/Login">  login</Link></p>
     </div>
