c2e249e24e7aec6ef2f5bc19543984e295be0e3c
leen <leenodeh287@gmail.com>
2023-11-01 22:07:53 +0300

    signup auth

--- src/Forms/Register.jsx
+++ src/Forms/Register.jsx
@@ -1,23 +1,61 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { ThemeContext } from '../Context/ThemeContext'
-
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../Context/ThemeContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/Config";
 
 const Register = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [name, setname] = useState("");
 
-  const {dark}=useContext(ThemeContext);
-  const c= dark ?'light' :'dark';
+  const { dark } = useContext(ThemeContext);
+  const c = dark ? "light" : "dark";
   return (
-    <div style={{height:"585px",textAlign:"center"}} className={c}>
+    <div style={{ height: "585px", textAlign: "center" }} className={c}>
       <p>Create an account</p>
-      <input type="text" placeholder='plz enter your name' required/>
+      <input
+        type="text"
+        placeholder="plz enter your name"
+        required
+        onChange={(e) => setname(e.target.value)}
+      />
 
-    <input type="text" placeholder='plz enter your email' required/>
-    <input type="password" placeholder='plz enter your password' required/>
-    <button >SignUp</button>
-<p> Already have an account<Link to="/Login">  login</Link></p>
+      <input
+        type="text"
+        placeholder="plz enter your email"
+        required
+        onChange={(e) => setemail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="plz enter your password"
+        required
+        onChange={(e) => setpassword(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              // Signed up
+              const user = userCredential.user;
+              // ...
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              // ..
+            });
+        }}
+      >
+        SignUp
+      </button>
+      <p>
+        {" "}
+        Already have an account<Link to="/Login"> login</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
