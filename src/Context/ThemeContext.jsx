57145d12394690765ff93b4195c37f0284fc8b0c
leen <leenodeh287@gmail.com>
2023-11-01 18:35:23 +0300

    done all

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,14 +1,23 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
-const ThemeContext = createContext(); // Fixed variable name
+const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
+  const savedTheme = localStorage.getItem("theme");
+  const [dark, setDark] = useState(savedTheme === "dark");
 
+  useEffect(() => {
+    // If there is no theme preference in localStorage, default to "light"
+    if (savedTheme === null) {
+      localStorage.setItem("theme", "light");
+    } else {
+      setDark(savedTheme === "dark");
+    }
+  }, [savedTheme]);
 
-  const [dark, setDark] = useState(false);
   const toggle = () => {
     setDark(!dark);
-    localStorage.setItem("theme",dark);
+    localStorage.setItem("theme", dark ? "light" : "dark");
   };
 
   return (
