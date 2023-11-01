fc6bdc74dbb2ead7df6432496e9b5953082b6c87
leen <leenodeh287@gmail.com>
2023-11-01 19:13:27 +0300

    darkMode form pages

--- src/Forms/Login.jsx
+++ src/Forms/Login.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React ,{useContext} from 'react'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../Context/ThemeContext'
+
 
 const Login = () => {
+  
+  const {dark}=useContext(ThemeContext);
+  const c= dark ?'light' :'dark';
   return (
-    <div style={{height:"585px",textAlign:"center"}}>
-    <input type="text" placeholder='plz enter your email' required/>
+    <div style={{height:"585px",textAlign:"center"}} className={c}>
+    <input type="text" placeholder='plz enter your email' required style={{marginTop:"0"}}/>
     <input type="password" placeholder='plz enter your password' required/>
     <button >LogIn</button>
 <p>Dont have an account <Link to="/Register">Register</Link></p>
--- src/Forms/Register.jsx
+++ src/Forms/Register.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../Context/ThemeContext'
 
 
 const Register = () => {
+
+  const {dark}=useContext(ThemeContext);
+  const c= dark ?'light' :'dark';
   return (
-    <div style={{height:"585px",textAlign:"center"}}>
+    <div style={{height:"585px",textAlign:"center"}} className={c}>
       <p>Create an account</p>
       <input type="text" placeholder='plz enter your name' required/>
 
